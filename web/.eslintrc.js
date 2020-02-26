1b791c80f71d9e630785cb4b1cd0982b7609105f
Vicent Gozalbes <vigosan@gmail.com>
2020-02-26 17:02:03 +0100

    Use js extension for hidden files

---
+++ web/.eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: './tsconfig.json',
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'eslint-config-prettier',
+  ],
+  rules: {
+    strict: ['error', 'never'],
+  },
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+    jest: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  overrides: [
+    {
+      files: '**/*.+(ts|tsx)',
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+      plugins: ['@typescript-eslint/eslint-plugin'],
+      extends: [
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'eslint-config-prettier/@typescript-eslint',
+      ],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': 'off',
+      },
+    },
+  ],
+};
