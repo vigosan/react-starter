0c8ccbb40656b11d4f820025a6b1fbecacb7bab5
Vicent Gozalbes <vigosan@gmail.com>
2020-10-04 19:55:59 +0200

    Upgrade dependencies

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,13 @@
-require('dotenv').config();
-
+import fs from 'fs';
+import dotenv from 'dotenv';
 import express, { Application } from 'express';
 
-const host = process.env.HOST;
-const port = process.env.PORT;
+dotenv.config();
 
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT;
 const mount = async (app: Application) => {
-  app.get('/health', function(req, res) {
+  app.get('/health', function (_, res) {
     res.send({ status: 'pass' });
   });
 
